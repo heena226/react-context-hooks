46d0809e724a859845fee7c148347056c9a9d113
Heena Taneja <heenataneja8@gmail.com>
2024-09-03 19:24:39 -0400

    Saving books data in LocalStorage

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 # Part-1 => Adding the books using the UseContext hook  
 
 # Part-2 => Using useReducer hook to modify the data
+
+# Part-3 => Using Local Storage
--- src/context/BooksContext.js
+++ src/context/BooksContext.js
@@ -1,11 +1,19 @@
-import React, {createContext, useReducer } from 'react';
+import React, {createContext, useReducer, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { bookReducer } from '../reducers/bookReducer';
 
 export const BooksContext = createContext();
 
 const BooksContextProvider = (props) => {
-    const [books, dispatch] = useReducer(bookReducer, []);
+    const [books, dispatch] = useReducer(bookReducer, [], () => {
+        const getBooks = localStorage.getItem('Books');
+        return (getBooks ? JSON.parse(getBooks): []);
+    });
+
+    useEffect(()=> {
+        localStorage.setItem('Books', JSON.stringify(books));
+    }, [books]);
+
     return (
         <BooksContext.Provider value={{books, dispatch}}>
             {props.children}
